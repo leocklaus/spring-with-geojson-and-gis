11d4475ec4467fe2fcfbe191d4bd32a8874579b5
leocklaus <leocklaus@gmail.com>
2024-08-01 10:44:29 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/ze_code_challenge_backend_gis/ZeCodeChallengeBackendGisApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.ze_code_challenge_backend_gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeCodeChallengeBackendGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeCodeChallengeBackendGisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/ze_code_challenge_backend_gis/ZeCodeChallengeBackendGisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.ze_code_challenge_backend_gis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeCodeChallengeBackendGisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
